06d11853cd184a5cafe3e1409edf74e986dc1976
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-07 10:02:28 -0600

    completes functional error test

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -12,9 +12,9 @@ class HomepageTest < CapybaraTestCase
     #It should attempt to visit some page that does not exist,
     #assert that we get a 404 status code (indicating that the client has made an error),
     #and assert that the page has some sort of message indicating that the page doesn't exist.
-    visit '/e'
+    visit '/foo'
 
-    assert page.has_content?("Oops, this page doesn't exist.")
+    assert page.has_content?("Page not found.")
     assert_equal 404, page.status_code
   end
 end
